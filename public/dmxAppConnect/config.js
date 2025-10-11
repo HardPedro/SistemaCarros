6f04724304d9dd28382963b54d4d48d9f167a77f
PedroHard <pedrohardsolu2025@gmail.com>
2025-10-11 20:15:07 -0300

    JINCARS v0.0.2 - PH

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,12 @@
+dmx.config({
+  "lista_produtos": {
+    "repeat2": {
+      "meta": [],
+      "outputType": "object"
+    },
+    "repeat1": {
+      "meta": [],
+      "outputType": "object"
+    }
+  }
+});
